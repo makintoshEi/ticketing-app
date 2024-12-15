fbcfe7e5b0d2fed9c661d101e8e7ff8c87f961e7
Asael <makintosh_ragga@yahoo.es>
2024-12-15 10:46:47 -0500

    feat: add basic layout setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,16 @@
+import TicketCard from "./(components)/ticket-card";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-5">
+      <div className="md:grid grid-cols-3 xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
