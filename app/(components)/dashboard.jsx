053092e6647c68a242eb7dc6118383ccd1437028
Asael <makintosh_ragga@yahoo.es>
2024-12-15 14:16:26 -0500

    refactor: add <Dashboard /> to initial page

---
+++ app/(components)/dashboard.jsx
@@ -0,0 +1,23 @@
+import TicketCard from "./ticket-card";
+
+const getTickets = async () => {
+  const tickets = await fetch("http://localhost:3000/api/tickets", {
+    method: "GET",
+    cache: "no-store",
+  });
+  return tickets.json();
+};
+
+const Dashboard = async () => {
+  const { data } = await getTickets();
+  return (
+    <div className="p-5">
+      <div className="md:grid grid-cols-3 xl:grid-cols-4">
+        {data &&
+          data.map((ticket, idx) => <TicketCard key={idx} {...ticket} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,7 @@
-import TicketCard from "./(components)/ticket-card";
+import Dashboard from "./(components)/dashboard";
 
-const getTickets = async () => {
-  const tickets = await fetch("http://localhost:3000/api/tickets", {
-    method: "GET",
-    cache: "no-store",
-  });
-  return tickets.json();
+const page = () => {
+  return <Dashboard />;
 };
 
-const Dashboard = async () => {
-  const { data } = await getTickets();
-  return (
-    <div className="p-5">
-      <div className="md:grid grid-cols-3 xl:grid-cols-4">
-        {data &&
-          data.map((ticket, idx) => <TicketCard key={idx} {...ticket} />)}
-      </div>
-    </div>
-  );
-};
-
-export default Dashboard;
+export default page;
