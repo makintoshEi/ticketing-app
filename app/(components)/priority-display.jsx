b903cdc439b08f66deb236ca2afb78fa3139aa79
Asael <makintosh_ragga@yahoo.es>
2024-12-15 14:33:10 -0500

    feat: add color to <PriorityDisplay /> and <StatusDisplay />

--- app/(components)/priority-display.jsx
+++ app/(components)/priority-display.jsx
@@ -5,7 +5,11 @@ const PriorityDisplay = ({ priorityNumber }) => {
   return (
     <div className="flex justify-start align-baseline">
       {Array.from({ length: priorityNumber }).map((_, index) => (
-        <FontAwesomeIcon key={index} icon={faFire} className="text-red-400" />
+        <FontAwesomeIcon
+          key={index}
+          icon={faFire}
+          className={`${priorityNumber > 2 ? "text-red-400" : "text-gray-400"}`}
+        />
       ))}
     </div>
   );
--- app/(components)/status-display.jsx
+++ app/(components)/status-display.jsx
@@ -1,6 +1,22 @@
 const StatusDisplay = ({ status }) => {
+  const getColor = (status) => {
+    switch (status) {
+      case "not started":
+        return "bg-red-200";
+      case "in progress":
+        return "bg-blue-200";
+      case "done":
+        return "bg-green-200";
+      default:
+        return "bg-yellow-200";
+    }
+  };
   return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200">
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(
+        status
+      )}`}
+    >
       {status}
     </span>
   );
