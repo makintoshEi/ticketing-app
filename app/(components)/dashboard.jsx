a926a3e0c158dbac24dde835d814e44ba614c72d
Asael <makintosh_ragga@yahoo.es>
2024-12-15 14:23:19 -0500

    feat: filter by category

--- app/(components)/dashboard.jsx
+++ app/(components)/dashboard.jsx
@@ -10,12 +10,26 @@ const getTickets = async () => {
 
 const Dashboard = async () => {
   const { data } = await getTickets();
+
+  const uniqueCategories = Array.from(
+    new Set(data.map((ticket) => ticket.category))
+  );
+
   return (
     <div className="p-5">
-      <div className="md:grid grid-cols-3 xl:grid-cols-4">
-        {data &&
-          data.map((ticket, idx) => <TicketCard key={idx} {...ticket} />)}
-      </div>
+      {data &&
+        uniqueCategories.map((category, categoryIdx) => (
+          <div key={categoryIdx} className="mb-4">
+            <h2>{category}</h2>
+            <div className="md:grid grid-cols-3 xl:grid-cols-4">
+              {data
+                .filter((t) => t.category === category)
+                .map((ticket, idx) => (
+                  <TicketCard key={idx} {...ticket} />
+                ))}
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
