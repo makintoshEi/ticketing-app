837da1b36675f8d9e292a0e2a63dbc448cf446b9
Asael <makintosh_ragga@yahoo.es>
2024-12-15 14:36:46 -0500

    feat: format time in <TicketCard />

--- app/(components)/ticket-card.jsx
+++ app/(components)/ticket-card.jsx
@@ -12,6 +12,17 @@ const TicketCard = ({
   title,
   createdAt,
 }) => {
+  const formatTimestamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+    return new Date(timestamp).toLocaleDateString("en-US", options);
+  };
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
@@ -26,7 +37,7 @@ const TicketCard = ({
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex- flex-col">
-          <p className="text-xs my-1">{createdAt}</p>
+          <p className="text-xs my-1">{formatTimestamp(createdAt)}</p>
           <ProgressBar progress={progress} />
         </div>
         <div className="ml-auto flex items-end">
