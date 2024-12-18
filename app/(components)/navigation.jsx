3abc23598a5c72dc882a5a030e2c685a0a19713d
Asael <makintosh_ragga@yahoo.es>
2024-12-18 12:27:58 -0500

    refactor: use <Image /> from next/image for better performance

--- app/(components)/navigation.jsx
+++ app/(components)/navigation.jsx
@@ -2,6 +2,7 @@
 import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import Image from "next/image";
 import { useAuth } from "@/app/(context)/auth-context";
 
 const Navigation = () => {
@@ -17,9 +18,16 @@ const Navigation = () => {
         </Link>
       </div>
       <div className="flex items-center space-x-4">
-        <img
+        <Image
           src={user.photoURL}
           alt="Profile"
+          width={40}
+          height={40}
+          style={{
+            borderRadius: "border-radius: 9999px",
+            width: "2.5rem",
+            height: "2.5rem",
+          }}
           className="w-10 h-10 rounded-full"
         />
         <p className="text-default-text">{user.email}</p>
--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
     MONG_DB_URI: process.env.MONG_DB_URI,
     ENVIRONMENT: process.env.ENVIRONMENT,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
