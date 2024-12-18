a24ddd363efa4ce07239cf5b3eafae103dd81107
Asael <makintosh_ragga@yahoo.es>
2024-12-17 20:35:46 -0500

    feat: add createSession fn which set the cookie on the SERVER

--- app/(lib)/session.js
+++ app/(lib)/session.js
@@ -1,5 +1,6 @@
 import "server-only";
 import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
 
 const secretKey = process.env.SESSION_SECRET;
 const encodeKey = new TextEncoder().encode(secretKey);
@@ -41,4 +42,18 @@ export async function decrypt(session) {
   } catch (error) {
     console.log("Failed to verify session");
   }
+}
+
+export async function createSession(userId) {
+  const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000);
+  const session = await encrypt({ userId, expiresAt });
+  const cookieStore = await cookies();
+
+  cookieStore.set("session", {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
 }
